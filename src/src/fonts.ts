3937560bfc9f7b9e3a9571071ad1db7e017b64a5
ichise38-svg <ichise38@gmail.com>
2026-08-30 17:48:42 +0900

    Delete src/src/fonts.ts

--- src/src/fonts.ts
+++
@@ -1,6 +0,0 @@
-import {loadFont} from '@remotion/google-fonts/NotoSansJP';
-
-export const {fontFamily: japaneseFontFamily} = loadFont('normal', {
-	weights: ['400', '500', '700', '900'],
-	subsets: ['japanese'],
-});
